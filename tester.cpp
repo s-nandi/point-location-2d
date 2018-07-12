1a24a5689651a77bb96a25f3f9044a0e9c3a8b78
Saujas Nandi <saujas.nandi@gmail.com>
2018-07-12 22:08:21 +0000

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include <vector>
 
-#include "point.h"
-#include "quadedge.h"
+#include "vertex.h"
+#include "plane.h"
 
 int main()
 {
     int n;
-    std::cin>>n;
+    std::cin >> n;
 
     std::vector <point> points(n);
     for (int i = 0; i < n; i++)
     {
-        std::cin>>points[i];
+        std::cin >> points[i];
     }
+
+    int m;
+    std::cin >> m;
+    std::vector <std::vector<int>> triangles(m);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        std::cin >> a >> b >> c;
+        triangles[i] = {a, b, c};
+    }
+
+    plane p;
+    edge* startEdge = p.init_subdivision(points, triangles);
+    debug(std::cout, p);
+
+    /*
+    plane p;
+
+    p.init_polygon({{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}, {0, 2}}, 5);
+    debug(std::cout, p);
+    */
+    //edge* startingEdge = p.init_triangle({0, 0}, {1, 1}, {0, 1}, 3);
+
 
     return 0;
 }
